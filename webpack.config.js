c160f2b9fb92c68d35ce1e28765ed9470985fdbb
Matheus Lima <matheusml90@gmail.com>
2016-03-10 09:51:12 -0300

    Changing build system

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,38 @@
 
 var webpack = require('webpack')
 
+var env = process.env.NODE_ENV
+
 var config = {
-    entry: [
-        './src/pareto.js'
-    ],
-    output: {
-        path: __dirname + '/dist',
-        filename: "pareto.min.js",
-        library: 'paretojs',
-        libraryTarget: 'umd'
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                pure_getters: true,
-                unsafe: true,
-                unsafe_comps: true,
-                screw_ie8: true,
-                warnings: false
-            }
-        })
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
     ]
+  },
+  output: {
+    library: 'Pareto',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
 };
 
-module.exports = config;
+if (env === 'production') {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  )
+}
+
+module.exports = config
