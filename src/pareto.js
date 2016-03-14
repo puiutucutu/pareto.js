1f832d99700bcfef0edcd3a778c7e5d5ca6eba4c
Matheus Lima <matheusml90@gmail.com>
2016-03-14 20:47:23 -0300

    Adding default export

--- src/pareto.js
+++ src/pareto.js
@@ -2,36 +2,21 @@ import ArrayUtils from './utils/array'
 import FunctionalUtils from './utils/functional'
 import ObjectUtils from './utils/object'
 
-const head = ArrayUtils.head
-const tail = ArrayUtils.tail
-const last = ArrayUtils.last
-const flatten = ArrayUtils.flatten
-const remove = ArrayUtils.remove
-const indexOf = ArrayUtils.indexOf
-const where = ArrayUtils.where
-const curry = FunctionalUtils.curry
-const compose = FunctionalUtils.compose
-const debounce = FunctionalUtils.debounce
-const memoize = FunctionalUtils.memoize
-const throttle = FunctionalUtils.throttle
-const equals = ObjectUtils.equals
-const matches = ObjectUtils.matches
-
 const Pareto = {
-    head,
-    tail,
-    last,
-    flatten,
-    remove,
-    indexOf,
-    where,
-    curry,
-    compose,
-    memoize,
-    debounce,
-    throttle,
-    equals,
-    matches
+    head: ArrayUtils.head,
+    tail: ArrayUtils.tail,
+    last: ArrayUtils.last,
+    flatten: ArrayUtils.flatten,
+    remove: ArrayUtils.remove,
+    indexOf: ArrayUtils.indexOf,
+    where: ArrayUtils.where,
+    curry: FunctionalUtils.curry,
+    compose: FunctionalUtils.compose,
+    memoize: FunctionalUtils.memoize,
+    debounce: FunctionalUtils.debounce,
+    throttle: FunctionalUtils.throttle,
+    equals: ObjectUtils.equals,
+    matches: ObjectUtils.matches
 }
 
 export default Pareto
