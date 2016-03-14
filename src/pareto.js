5bfffc7e042262817abf5d66835069873f361170
Matheus Lima <matheusml90@gmail.com>
2016-03-14 19:35:04 -0300

    Changing export style

--- src/pareto.js
+++ src/pareto.js
@@ -2,21 +2,34 @@ import ArrayUtils from './utils/array'
 import FunctionalUtils from './utils/functional'
 import ObjectUtils from './utils/object'
 
-const Pareto = {
-    head: ArrayUtils.head,
-    tail: ArrayUtils.tail,
-    last: ArrayUtils.last,
-    flatten: ArrayUtils.flatten,
-    remove: ArrayUtils.remove,
-    indexOf: ArrayUtils.indexOf,
-    where: ArrayUtils.where,
-    curry: FunctionalUtils.curry,
-    compose: FunctionalUtils.compose,
-    memoize: FunctionalUtils.memoize,
-    debounce: FunctionalUtils.debounce,
-    throttle: FunctionalUtils.throttle,
-    equals: ObjectUtils.equals,
-    matches: ObjectUtils.matches
-}
+const head = ArrayUtils.head
+const tail = ArrayUtils.tail
+const last = ArrayUtils.last
+const flatten = ArrayUtils.flatten
+const remove = ArrayUtils.remove
+const indexOf = ArrayUtils.indexOf
+const where = ArrayUtils.where
+const curry = FunctionalUtils.curry
+const compose = FunctionalUtils.compose
+const debounce = FunctionalUtils.debounce
+const memoize = FunctionalUtils.memoize
+const throttle = FunctionalUtils.throttle
+const equals = ObjectUtils.equals
+const matches = ObjectUtils.matches
 
-export default Pareto
+export default {
+    head,
+    tail,
+    last,
+    flatten,
+    remove,
+    indexOf,
+    where,
+    curry,
+    compose,
+    memoize,
+    debounce,
+    throttle,
+    equals,
+    matches
+}
