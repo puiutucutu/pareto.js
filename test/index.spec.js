7910be518d260c82f5da340474f5b967a55a3984
Matheus Lima <matheusml90@gmail.com>
2016-07-27 11:53:11 -0300

    Changing file names

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 import expect from 'expect'
-import _ from '../src/pareto'
+import _ from '../src/index'
 
 describe('Pareto', () => {
     describe('init', () => {
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,7 +3,7 @@ import {join} from 'path'
 const include = join(__dirname, 'src')
 
 export default {
-  entry: './src/pareto',
+  entry: './src/index',
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
