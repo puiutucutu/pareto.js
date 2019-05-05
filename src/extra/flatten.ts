b9d53864b22eb6ddbef3c1672395edee67a8ba22
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-05-05 12:11:39 -0400

    Update flatten.ts

--- src/extra/flatten.ts
+++ src/extra/flatten.ts
@@ -1,6 +1,15 @@
-export default function flatten<T>(array: Array<T>): Array<T> {
+
+const concact xs => y = Array.prototype.concat.call(xs, y);
+const isArray x => => Array.isArray(x);
+const concatenable = x => isArray(x) ? true : false;
+const doWhenConcatenable => f => x => concatenable(x) ? f(x) : x;
+
+function flatten<T>(array: Array<T>): Array<T> {
   return array.reduce(
+    (a, b) => concat(a)(doWhenConcatenable(flatten)(b))
     (a, b) => a.concat(Array.isArray(b) ? flatten(b) : b),
     []
   );
 }
+
+export default flatten,
